8724d053a5d16aa07384e43b72b3c520957d8c84
Egon Firman <egon.firman@gmail.com>
2019-07-21 08:47:27 +0700

    remove unused function

--- query_generator.go
+++ query_generator.go
@@ -5,29 +5,11 @@ import (
 	"reflect"
 )
 
-// generateInsertQuery query generator
-// make sure that entity that got into these functions is a struct type
-func generateInsertQuery(entity reflect.Type) string {
-	tableName := ToSnakeCase(entity.Name())
-	columnsSelect := ""
-	fills := ""
+/*
+generateSelectQuery
 
-	for i := 1; i < entity.NumField(); i++ {
-		name := ToSnakeCase(entity.Field(i).Name)
-		if i == 1 {
-			columnsSelect += name
-			fills += fmt.Sprintf("$%d", i)
-		} else {
-			columnsSelect += ", " + name
-			fills += fmt.Sprintf(", $%d", i)
-		}
-	}
-
-	return fmt.Sprintf("INSERT INTO %s(%s) VALUES (%s) RETURNING id", tableName, columnsSelect, fills)
-}
-
-// generateSelectQuery query generator
-// make sure that entity that got into these functions is a struct type
+generate string "SELECT column1, column2 FROM tableName"
+*/
 func generateSelectQuery(entity reflect.Type) string {
 	tableName := ToSnakeCase(entity.Name())
 	columnsSelect := ""
@@ -39,8 +21,41 @@ func generateSelectQuery(entity reflect.Type) string {
 			columnsSelect += ", " + name
 		}
 	}
-
 	return fmt.Sprintf("SELECT %s FROM %s LIMIT $1 OFFSET $2", columnsSelect, tableName)
+}
+
+/*
+generateSelectQueryWithout
+
+generate string "SELECT column1 FROM tableName" but remove the filtered column
+*/
+func generateSelectQueryWithout(entity reflect.Type, without []string) string {
+	tableName := ToSnakeCase(entity.Name())
+	fieldNames := readEntityFieldsName(entity)
+	filteredFieldNames := filterFieldNames(fieldNames, without)
+	columnsSelect := generateColumnNames(filteredFieldNames)
+	return fmt.Sprintf("SELECT %s FROM %s LIMIT $1 OFFSET $2", columnsSelect, tableName)
+}
+
+// generateInsertQuery query generator
+// make sure that entity that got into these functions is a struct type
+func generateInsertQuery(entity reflect.Type) string {
+	tableName := ToSnakeCase(entity.Name())
+	fieldNames := readEntityFieldsName(entity)
+
+	filteredFieldNames := filterFieldNamesWithFunc(
+		fieldNames,
+		func(n string, i int) bool { return i != 0 },
+	)
+	columnsSelect := generateColumnNames(filteredFieldNames)
+
+	fillsArray := make([]string, len(filteredFieldNames))
+	for i := 1; i < entity.NumField(); i++ {
+		fillsArray[i-1] = fmt.Sprintf("$%d", i)
+	}
+	fills := generateColumnNames(fillsArray)
+
+	return fmt.Sprintf("INSERT INTO %s(%s) VALUES (%s) RETURNING id", tableName, columnsSelect, fills)
 }
 
 // generateUpdateQuery query generator
@@ -66,4 +81,80 @@ func generateUpdateQuery(entity reflect.Type) string {
 func generateDeleteQuery(entity reflect.Type) string {
 	tableName := ToSnakeCase(entity.Name())
 	return fmt.Sprintf("DELETE FROM %s WHERE id = $1", tableName)
+}
+
+func stringArrayContains(stack []string, needle string) bool {
+	for _, s := range stack {
+		if s == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func readEntityFieldsName(entity reflect.Type) []string {
+	num := entity.NumField()
+	result := make([]string, num)
+	for i := 0; i < num; i++ {
+		result[i] = entity.Field(i).Name
+	}
+	return result
+}
+
+func generateColumnNames(names []string) string {
+	columnsSelect := ""
+	for i, name := range names {
+		name := ToSnakeCase(name)
+
+		if len(name) < 1 {
+			continue
+		}
+
+		if i == 0 {
+			columnsSelect += name
+		} else {
+			columnsSelect += ", " + name
+		}
+	}
+	return columnsSelect
+}
+
+func joinToString(fields []string) string {
+	result := ""
+	for i, field := range fields {
+		if i == 0 {
+			result += field
+		} else {
+			result += ", " + field
+		}
+	}
+	return result
+}
+
+func filterFieldNames(fieldNames []string, without []string) []string {
+	result := make([]string, len(fieldNames))
+	i := 0
+	for _, name := range fieldNames {
+		if stringArrayContains(without, ToSnakeCase(name)) {
+			continue
+		}
+		result[i] = name
+		i += 1
+	}
+	return result
+}
+
+func filterFieldNamesWithFunc(
+	fieldNames []string,
+	filterFunc func(string, int) bool,
+) []string {
+	result := make([]string, len(fieldNames))
+	i := 0
+	for index, name := range fieldNames {
+		if filterFunc(name, index) {
+			result[i] = name
+			i += 1
+		}
+	}
+	return result
 }
